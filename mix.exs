9fc637905d3bc31ca8a7ca905af72f68e3960bf1
iwada <iwada@twalle.com>
2019-02-06 04:20:58 +0100

    Removed edeliver from app list

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Ivr.MixProject do
   def application do
     [
       mod: {Ivr.Application, []},
-      extra_applications: [:logger, :runtime_tools,:edeliver]
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
